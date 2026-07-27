0496b63d7efa106acb689d7b6d8839fa39a6b44c
Tsulukidze <temur.tsulukidze@gmail.com>
2026-07-27 13:13:28 +0300

    Convert the nodes to a class-based structure

--- lesson1/nodes/publisher.py
+++ lesson1/nodes/publisher.py
@@ -3,10 +3,17 @@
 import rospy
 from std_msgs.msg import String
 
-rospy.init_node('publisher')
-rate = rospy.Rate(2)
-pub = rospy.Publisher('/message', String, queue_size=10)
+class Publisher:
+    def __init__(self):
+        # Internal variables
+        self.rate = ...
 
-while not rospy.is_shutdown():
-    pub.publish("Hello world!")
-    rate.sleep()
+        # Publishers
+        self.pub = ...
+
+    def run(self):
+        while not rospy.is_shutdown():
+            ...
+
+if __name__ == '__main__':
+    ...
